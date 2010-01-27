fae714ccbeb94e43aeafeb99808b99f7c09a5f91
Matthew Goff <matt@goff.cc>
2010-01-27 07:58:53 +0000

    AreaConvert is now built into its own repository.

--- src/areaconvert/dependencies.d
+++
@@ -1,8 +0,0 @@
-o / ack431.o: ack431.c h / areaconvert.h h / include.h h / ack431.h h / ackfuss.h \
-h / flags.h h / extern.h
-o / ackfuss.o: ackfuss.c h / areaconvert.h h / include.h h / ack431.h h / ackfuss.h \
-h / flags.h h / extern.h
-o / areaconvert.o: areaconvert.c h / areaconvert.h h / include.h h / ack431.h \
-h / ackfuss.h h / flags.h
-o / flags.o: flags.c h / areaconvert.h h / include.h h / ack431.h h / ackfuss.h \
-h / flags.h h / extern.h
--- src/areaconvert/h/rom246b.h
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright Matt Goff (Kline) 2009
- * If you use my code, please give credit where it is due.
- * Support provided at www.ackmud.net
- */
-
-#include "include.h"
